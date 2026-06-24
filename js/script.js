418d19c56032b6b13f6ce32870b62228396128d3
Maniek Gaznik <maniekgaznik@gmail.com>
2026-06-24 17:06:01 +0200

    konsola link klik

--- js/script.js
+++ js/script.js
@@ -3,25 +3,35 @@
 const titleClickHandler = function(event) {
   event.preventDefault();
 
+  console.log('Link was clicked!');
+  console.log(event);
+
   const clickedElement = this;
 
+  /* remove class 'active' from all article links */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for (const activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
+  /* add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
+  /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
 
   for (const activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
+  /* get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
+
+  /* find the correct article using the selector */
   const targetArticle = document.querySelector(articleSelector);
 
+  /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
 };
 
@@ -30,13 +40,3 @@ const links = document.querySelectorAll('.titles a');
 for (const link of links) {
   link.addEventListener('click', titleClickHandler);
 }
-
-function testLinksHandler() {
-  const links = document.querySelectorAll('.titles a');
-
-  console.log(links);
-}
-
-const buttonTest = document.getElementById('button-test');
-
-buttonTest.addEventListener('click', testLinksHandler);
