02074bce66e6268cc96e6b1ebcabeb9bdb055b87
Maniek Gaznik <maniekgaznik@gmail.com>
2026-06-24 16:52:50 +0200

    naprawa css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('Script loaded');
+const titleClickHandler = function(event) {
+  event.preventDefault();
+
+  const clickedElement = this;
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for (const activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+
+  clickedElement.classList.add('active');
+
+  const activeArticles = document.querySelectorAll('.post.active');
+
+  for (const activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
+
+  const articleSelector = clickedElement.getAttribute('href');
+  const targetArticle = document.querySelector(articleSelector);
+
+  targetArticle.classList.add('active');
+};
+
+const links = document.querySelectorAll('.titles a');
+
+for (const link of links) {
+  link.addEventListener('click', titleClickHandler);
+}
